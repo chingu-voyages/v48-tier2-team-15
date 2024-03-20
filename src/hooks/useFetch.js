53a7b81e5c61cf64105f182a3892598e323620f2
Geoff Jamieson <geoff.ijamieson@gmail.com>
2024-03-20 17:23:24 -0400

    create custom hook for fetching data

---
+++ src/hooks/useFetch.js
@@ -0,0 +1,33 @@
+import { useEffect, useState } from "react";
+
+export const useFetch = (url) => {
+  const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch(url);
+        if (!response.ok) {
+          throw new Error("Bad network response");
+        }
+        const result = await response.json();
+        setData(result);
+      } catch (error) {
+        setError(error);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchData();
+
+    return () => {
+      const controller = new AbortController();
+      controller.abort();
+    };
+  }, [url]);
+
+  return { data, loading, error };
+};
