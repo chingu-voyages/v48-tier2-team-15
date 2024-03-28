1b34adfc6b24d24017697bbd10392ca3708882df
Geoff Jamieson <geoff.ijamieson@gmail.com>
2024-03-28 00:08:54 -0400

    add scroll to top feature

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,16 @@
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import { Outlet } from "react-router-dom";
+import { useLocation } from "react-router-dom";
+import { useEffect } from "react";
 
 const App = () => {
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
+
   return (
     <>
       <Header />
