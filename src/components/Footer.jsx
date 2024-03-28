6a874a493f843dc2cba60e033156595bfac0a81b
Geoff Jamieson <geoff.ijamieson@gmail.com>
2024-03-28 00:15:14 -0400

    fix footer bg

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,12 +3,12 @@ import { faGithub } from "@fortawesome/free-brands-svg-icons";
 
 const Footer = () => {
   return (
-    <footer className="w-full py-10 text-center">
+    <footer className="w-full py-10 text-center bg-black">
       <a
         href="https://github.com/chingu-voyages/v48-tier2-team-15"
         target="_blank"
       >
-        <FontAwesomeIcon icon={faGithub} color="black" size="2xl" />
+        <FontAwesomeIcon icon={faGithub} color="white" size="2xl" />
       </a>
     </footer>
   );
