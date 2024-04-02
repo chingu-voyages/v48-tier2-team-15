bdf04616d96a7f9c256f179b7ab82d64d07b0877
Geoff Jamieson <geoff.ijamieson@gmail.com>
2024-04-02 16:46:31 -0400

    minor style change to footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import { faGithub } from "@fortawesome/free-brands-svg-icons";
 
 const Footer = () => {
   return (
-    <footer className="w-full py-8 text-center bg-black">
+    <footer className="w-full p-6 text-center bg-slate-700">
       <a
         href="https://github.com/chingu-voyages/v48-tier2-team-15"
         target="_blank"
