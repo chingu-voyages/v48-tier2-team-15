d2199e3c6cc3a0a40a12ee37e1fd56f6a0c5434b
Geoff Jamieson <geoff.ijamieson@gmail.com>
2024-04-02 16:46:03 -0400

    general style fixes to home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,16 +1,14 @@
 import Hero from "../components/Hero";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faCircleInfo, faMap } from "@fortawesome/free-solid-svg-icons";
-import { Link } from "react-router-dom";
-import { faBarChart } from "@fortawesome/free-regular-svg-icons";
+import NavCard from "../components/NavCard";
+import { navCardData } from "../data/navCardData";
 
 const Home = () => {
   return (
     <main>
       <Hero />
       {/* Some General Info */}
-      <div className="w-[80%] max-w-[800px] mx-auto my-12">
-        <h3 className="text-xl font-firaSans mb-4">Lorem Ipsum</h3>
+      <div className="w-[80%] max-w-[800px] mx-auto my-12 mb-20">
+        <h3 className="text-xl mb-4 font-bold">Lorem Ipsum</h3>
         <p>
           Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolores sit
           autem dolore hic voluptatem porro cum sapiente accusamus eos quis
@@ -19,32 +17,16 @@ const Home = () => {
         </p>
       </div>
       {/* Nav Cards */}
-      <div className="w-[80%] max-w-[1200px] mx-auto grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-3 gap-8 mb-20 text-center">
-        <Link to="/go-digging">
-          <div className="border-black border-2 p-10 max-w-[400px] min-h-[250px] mx-auto">
-            <FontAwesomeIcon icon={faMap} size="2xl" className="mb-6" />
-            <h4 className="font-bold text-xl font-firaSans mb-4">Go Digging</h4>
-            <p className="text-sm">Check out our interactive dinosaur map!</p>
-          </div>
-        </Link>
-
-        <Link to="/data">
-          <div className="border-black border-2 p-10 max-w-[400px] min-h-[250px] mx-auto">
-            <FontAwesomeIcon icon={faBarChart} size="2xl" className="mb-6" />
-            <h4 className="font-bold text-xl font-firaSans mb-4">Dino Data</h4>
-            <p className="text-sm">Learn more about dinosaur diets & types</p>
-          </div>
-        </Link>
-
-        <Link to="/about">
-          <div className="border-black border-2 p-10 max-w-[400px] min-h-[250px] mx-auto">
-            <FontAwesomeIcon icon={faCircleInfo} size="2xl" className="mb-6" />
-            <h4 className="font-bold text-xl font-firaSans mb-4">About Us</h4>
-            <p className="text-sm">
-              Interested in knowing more about the project?
-            </p>
-          </div>
-        </Link>
+      <div className="w-[80%] max-w-[1200px] mx-auto grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-3 gap-8 mb-24 text-center">
+        {navCardData.map(({ title, description, to, icon }, index) => (
+          <NavCard
+            key={index}
+            title={title}
+            description={description}
+            to={to}
+            icon={icon}
+          />
+        ))}
       </div>
     </main>
   );
