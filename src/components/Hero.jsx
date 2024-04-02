8d3a8a42fe5b01ee2456f2ffcf19bbc8bd3275b2
Geoff Jamieson <geoff.ijamieson@gmail.com>
2024-04-02 16:45:47 -0400

    hero style fixes

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,7 +2,7 @@ import heroDinoPic from "../assets/dinosaurs.png";
 
 const Hero = () => {
   return (
-    <div className="relative">
+    <div className="relative -z-10">
       <img
         src={heroDinoPic}
         alt="Several dinosaurs standing near a lake, with lush greenery in the background"
@@ -10,11 +10,11 @@ const Hero = () => {
       />
       <div className="absolute inset-0 bg-black bg-opacity-50 flex justify-center items-center p-4">
         <div className="text-center">
-          <h1 className="text-4xl md:text-6xl text-white font-bold mb-4 font-firaSans">
-            Welcome to DinoWorld
+          <h1 className="text-4xl md:text-6xl text-white font-bold mb-4">
+            Welcome to DinoPedia
           </h1>
           <p className="md:text-lg text-white">
-            A place to discover all sorts of cool facts about dinosaurs!
+            A place to discover all sorts of cool facts about dinosaurs
           </p>
         </div>
       </div>
