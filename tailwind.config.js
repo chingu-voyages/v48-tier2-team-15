5aa0f2ea4b912e7ced9c965c85da0d950dcabfed
Geoff Jamieson <geoff.ijamieson@gmail.com>
2024-04-02 16:38:20 -0400

    style fixes & add logo

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,11 +2,7 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    fontFamily: {
-      mountainsOfChristmas: ["Mountains of Christmas", "serif"],
-      montserrat: ["Montserrat", "sans-serif"],
-      firaSans: ["Fira Sans", "sans-serif"],
-    },
+    fontFamily: {},
     extend: {},
   },
   plugins: [],
