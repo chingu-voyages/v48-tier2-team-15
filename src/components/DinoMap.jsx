281ae9a1e511afb67384bf7347a6ca25cb7e1799
mkg1 <gehring.mg@gmail.com>
2024-04-03 12:12:42 -0400

    Custom geocode hook + code comments

--- src/components/DinoMap.jsx
+++ src/components/DinoMap.jsx
@@ -2,33 +2,38 @@ import React, {useCallback, useState} from 'react'
 import { GoogleMap, useJsApiLoader, Marker } from '@react-google-maps/api';
   
 const DinoMap = ({markers}) => {
+
+    // function from @react-google-maps/api package. Pass in API key from .env file
     const { isLoaded } = useJsApiLoader({
         id: 'google-map-script',
         googleMapsApiKey: import.meta.env.VITE_GOOGLE_MAPS_API_DEV
       })
 
-      const [dinoMap, setMap] = useState(null)
+    const [dinoMap, setMap] = useState(null)
 
-      const center = {
-        lat:  0,
-        lng: 0
-      };
-    
-      const containerStyle = {
-        width: '80%',
-        margin: 'auto',
-        height: '400px'
-      };
-          
-      const onLoad = useCallback(function callback(dinoMap) {    
-        setMap(dinoMap)
-      }, [])
-    
-      const onUnmount = useCallback(function callback(dinoMap) {
-        setMap(null)
-      }, [])
-    
-      console.log(markers)
+    // set the initial center of map  
+    const center = {
+    lat:  0,
+    lng: 0
+    };
+
+    // set map container style
+    const containerStyle = {
+    width: '80%',
+    margin: 'auto',
+    height: '400px'
+    };
+        
+    // what to do when map obj has loaded
+    const onLoad = useCallback(function callback(dinoMap) {    
+    setMap(dinoMap)
+    }, [])
+
+    const onUnmount = useCallback(function callback(dinoMap) {
+    setMap(null)
+    }, [])
+
+    // if map is loaded, display it with passed-in markers prop
     return isLoaded ? (
         <GoogleMap
             mapContainerStyle={containerStyle}
@@ -38,7 +43,7 @@ const DinoMap = ({markers}) => {
             onUnmount={onUnmount}
         >
             {markers && markers.map((marker, index) => (
-            <Marker key={index} position={marker} label={marker.label} />
+                <Marker key={index} position={marker} label={marker.label} />
             ))}
         </GoogleMap>
     ) : <></>
--- src/pages/DinosaurDetailView.jsx
+++ src/pages/DinosaurDetailView.jsx
@@ -1,16 +1,25 @@
 import { useParams } from "react-router-dom";
 import DinoMap from '../components/DinoMap'
 import { useFetch } from "../hooks/useFetch";
+import { useGeocode } from "../hooks/useGeocode";
 
 
 const DinosaurDetail = () => {
   const { dinosaurName } = useParams();
+
+  // use useFetch hook to get details for a specific dinosaur (e.g., dino with id=1)
+  // returned data.foundIn value can be used to 
   const { data, error, loading } = useFetch(
     "https://chinguapi.onrender.com/dinosaurs/1"
   );
+  const foundIn = data["foundIn"]
 
-  // const foundIn = data["foundIn"]
-    // add hook to get geocoded location data
+  // useGeocode custom hook. Returns a geocodeAddress function that takes an address (e.g., country name) and returns geocode lat/long information
+  //can use returned coordinates to pass in as markers to the <DinoMap /> component 
+  const [coordinates, geocodeAddress] = useGeocode();
+
+  // probably will want to cache this information so it's not hitting the api each time this component renders
+  geocodeAddress(foundIn);
 
   const markers = [{"lat": -30.559482, "lng": 22.937506}]  
   return (
